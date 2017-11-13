f5db23be537ca143d5640e9d463ac709d4bf71ce
SytheMusic <clawschaospsn@gmail.com>
2017-11-12 20:07:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'kek') {
+    	message.reply('kek?');
   	}
 });
 
